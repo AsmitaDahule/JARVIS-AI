da612cb0d39ae418c861c3c833669be7c9bee8c9
Asmita Dahule <dahuleasmita@gmail.com>
2025-03-29 13:27:21 +0530

    some changes

--- JARVIS AI/index.js
+++ JARVIS AI/index.js
@@ -2,21 +2,20 @@ import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
 import bodyParser from "body-parser";
-import virtualRoute from "./routes/virtualRoute.js"
+import virtualRoute from "./routes/virtualRoute.js";
 
-dotenv.config();
 
-const app = express()
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-const PORT = process.env.PORT || 3000
 app.set("view engine", "ejs");
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.render("main");
-})
+});
 
 app.use('/jarvis', virtualRoute);
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`.bgGreen.white))
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`.bgGreen.white));
